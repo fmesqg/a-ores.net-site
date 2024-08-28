03c8ee40acfb2f0e22103c505f3a7b585c5e84c0
fmg <f0@outlook.pt>
2024-08-28 05:21:46 +0100

    (auto) update

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,18 +1,15 @@
-# from .fetch import fetch_current_state
-# from .utils import (
-#     append_state,
-#     compute_delta_ids,
-#     get_prev_state,
-#     write_post,
-# )
-
-from .retro_fix_posts import retro_fix_posts
+from .fetch import fetch_current_state
+from .utils import (
+    append_state,
+    compute_delta_ids,
+    get_prev_state,
+    write_post,
+)
 
 if __name__ == "__main__":
-    # current_state = fetch_current_state()
-    # previous_state = get_prev_state()
-    # delta = compute_delta_ids(previous_state, current_state)
-    # if delta:
-    #     append_state(current_state)
-    # write_post(delta)
-    retro_fix_posts()
+    current_state = fetch_current_state()
+    previous_state = get_prev_state()
+    delta = compute_delta_ids(previous_state, current_state)
+    write_post(delta)
+    if delta:
+        append_state(current_state)
